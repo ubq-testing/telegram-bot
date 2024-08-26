96ea2af94fc504b5ded3d4827f2f2725531c3441
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-26 21:53:40 +0100

    chore: add grammy and bot token env

--- src/types/env.ts
+++ src/types/env.ts
@@ -3,14 +3,9 @@ import { StaticDecode } from "@sinclair/typebox";
 import "dotenv/config";
 import { StandardValidator } from "typebox-validators";
 
-/**
- * Define sensitive environment variables here.
- *
- * These are fed into the worker/workflow as `env` and are
- * taken from either `dev.vars` or repository secrets.
- * They are used with `process.env` but are type-safe.
- */
-export const envSchema = T.Object({});
+export const envSchema = T.Object({
+    TG_BOT_TOKEN: T.String(),
+});
 
 export const envValidator = new StandardValidator(envSchema);
 
