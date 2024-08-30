64c84e4c146e3129601716be1d528bca15ca2c96
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-30 14:00:20 +0100

    chore: post workroom comment on issue

--- src/handlers/github/workrooms.ts
+++ src/handlers/github/workrooms.ts
@@ -1,9 +1,9 @@
-import { getTelegramBot } from "#root/utils/get-telegram-bot.js";
+import { TelegramBotSingleton } from "#root/utils/telegram-bot-single.js";
 import { Context } from "../../types";
 
 export async function createChatroom(context: Context) {
-    const { env, logger, config } = context;
-    const { bot } = await getTelegramBot(env);
+    const { logger, config } = context;
+    const bot = TelegramBotSingleton.getInstance().getBot();
     const title = context.payload.issue.title
 
     logger.info(`Creating chatroom for issue ${title}`);
@@ -16,6 +16,30 @@ export async function createChatroom(context: Context) {
         logger.error(`Failed to create chatroom for issue ${title}`, { er });
     }
 
-    return forum;
+    if (forum) {
+        await addCommentToIssue(context, `Workroom created: https://t.me/${config.supergroupChatName}/${forum?.message_thread_id}`);
+    } else {
+        await addCommentToIssue(context, logger.error(`Failed to create chatroom for issue ${title}`).logMessage.diff);
+    }
 }
 
+
+async function addCommentToIssue(context: Context, msg: string) {
+    const { logger, octokit } = context;
+    const { repository: { full_name }, issue } = context.payload;
+    const [owner, repo] = full_name.split("/");
+
+    logger.info(`Adding comment to issue ${issue.number}`);
+
+    try {
+        await octokit.issues.createComment({
+            owner,
+            repo,
+            issue_number: issue.number,
+            body: msg,
+        });
+        logger.info(`Added comment to issue ${issue.number}`);
+    } catch (er) {
+        logger.error(`Failed to add comment to issue ${issue.number}`, { er });
+    }
+}
